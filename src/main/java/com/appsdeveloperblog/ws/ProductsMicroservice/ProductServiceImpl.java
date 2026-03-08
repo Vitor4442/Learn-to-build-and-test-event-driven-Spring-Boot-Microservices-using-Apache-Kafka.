62700f31312db81aa11e7c88ba34169c9c9f19dd
José Vitor <jose.vitorw018@gmail.com>
2026-03-07 23:10:10 -0300

    Metodo que envia mensagem de maneira assincrona

--- src/main/java/com/appsdeveloperblog/ws/ProductsMicroservice/ProductServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/ws/ProductsMicroservice/ProductServiceImpl.java
@@ -1,8 +1,28 @@
 package com.appsdeveloperblog.ws.ProductsMicroservice;
 
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.UUID;
+
 public class ProductServiceImpl implements ProductService{
+
+    private final KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
+    public ProductServiceImpl(KafkaTemplate kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Override
     public String createProduct(CreateProductRestModel productRestModel) {
+
+        String productId = UUID.randomUUID().toString();
+
+        //TODO: Persist Product Details into database table before publishing an event
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId, productRestModel.getTitle(), productRestModel.getPrice(), productRestModel.getQuantity());
+
+        kafkaTemplate.send("products-created-events-topic", productId, productCreatedEvent);
+
         return "";
     }
 }
