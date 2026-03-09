69aa7716f7acaf3561e0f65590d4567d92f34286
José Vitor <jose.vitorw018@gmail.com>
2026-03-08 23:37:40 -0300

    Feito a tratação de erros e mostrado o jeito de fazer uma comunicação assincrona e sincrona

--- src/main/java/com/appsdeveloperblog/ws/ProductsMicroservice/ProductServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/ws/ProductsMicroservice/ProductServiceImpl.java
@@ -1,12 +1,17 @@
 package com.appsdeveloperblog.ws.ProductsMicroservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 public class ProductServiceImpl implements ProductService{
 
     private final KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     public ProductServiceImpl(KafkaTemplate kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
@@ -21,8 +26,18 @@ public class ProductServiceImpl implements ProductService{
 
         ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId, productRestModel.getTitle(), productRestModel.getPrice(), productRestModel.getQuantity());
 
-        kafkaTemplate.send("products-created-events-topic", productId, productCreatedEvent);
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> future;
+        future = kafkaTemplate.send("products-created-events-topic", productCreatedEvent);
 
+        future.whenComplete((result, exeption) -> {
+            if(exeption != null){
+                LOGGER.error("Failed to send message: " + exeption.getMessage());
+            } else {
+                LOGGER.info("Message sent sucessfully: " + result.getRecordMetadata());
+            }
+        });
+
+            future.join(); //esse pequeno trecho de código faz o processo ficar sincrono
         return "";
     }
 }
